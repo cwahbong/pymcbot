8dbb45bc438d811e1866d03be4d86af9d3bf768f
cwahbong <cw.ahbong@gmail.com>
2012-06-29 23:22:00 +0800

    Now it can unpack packets.

--- mc/packets.py
+++ mc/packets.py
@@ -1,53 +1,106 @@
 import struct
 
+
+_type_by_id = {}
+
+
+def register_packet_type(cls):
+  _type_by_id[cls.id] = cls
+  return cls
+
+
+def _get_datatable(cls, direction):
+  return getattr(cls, "data_{}".format(direction))
+
+
+def _get_packet_class(id):
+  return _type_by_id[id]
+
  
-def pack(packet):
+def pack(packet, direction):
   result = struct.pack("!B", packet.id)
-  for d in packet.get_datatable():
-    attr = getattr(packet, d[1], None)
-    if d[0] in ("ba", "Ba", "Ta"): # array
+  for type, kw in _get_datatable(packet.__class__, direction):
+    attr = getattr(packet, kw, None)
+    if type in ("ba", "Ba", "Ta"): # array
       raise NotImplementedError
-    if d[0]=="S": # utf 16 be string
+    if type=="S": # utf 16 be string
       attr = "" if attr is None else attr
       utf16_attr = attr.encode("utf_16_be")
       result += struct.pack("!h", len(attr))
       result += struct.pack("!{}s".format(len(utf16_attr)), utf16_attr)
-    elif d[0]=="T": # slot
+    elif type=="T": # slot
       raise NotImplementedError
-    elif d[0]=='M': # metadata
+    elif type=='M': # metadata
       raise NotImplementedError
-    elif d[0] in "bB?hHiIlLfd":
+    elif type in "bB?hHiIlLfd":
       attr = 0 if attr is None else attr
-      result += struct.pack("!{}".format(d[0]), attr)
+      result += struct.pack("!{}".format(type), attr)
     else:
-      raise ValueError("Invalid data type '{}'".format(d[0]))
+      raise ValueError("Invalid data type '{}'".format(type))
   return result
-  
+
+
 def unpack(rawstring, direction):
-  # 1. find all class in this module, match id.
-  # 2. unpack data with specific data table.
-  pass
+  # match id.
+  pack_id = struct.unpack_from("!B", rawstring)[0]
+  pack_class = _get_packet_class(pack_id)
+  offset = struct.calcsize("!B")
+  # unpack data with specific data table.
+  attr = {}
+  for type, kw in _get_datatable(pack_class, direction):
+    if type in ("ba", "Ba", "Ta"):
+      raise NotImplementedError("unpack array")
+    elif type=="S":
+      length = struct.unpack_from("!h", rawstring, offset)[0]
+      print "L", length, len(rawstring)
+      utf16_length = len((" "*length).encode("utf_16_be"))
+      offset += struct.calcsize("!h")
+      utf16_attr = struct.unpack_from("!{}s".format(utf16_length), rawstring, offset)[0]
+      offset += utf16_length
+      val = utf16_attr.decode("utf_16_be")
+    elif type=="T":
+      raise NotImplementedError("unpack slot")
+    elif type=="M":
+      raise NotImplementedError("unpack metadata")
+    elif type in "bB?hHiIlLfd":
+      print "O", struct.calcsize(type)
+      val = struct.unpack_from("!{}".format(type), rawstring, offset)[0]
+      offset += struct.calcsize(type)
+    else:
+      raise ValueError("Invalid data type '{}'".format(type))
+    if kw:
+      attr[kw] = val
+  # construct packet object
+  return pack_class(**attr)
 
 
 class packet(object):
   id = -1
 
-  def __init__(self, direction, **kwargs):
-    if direction not in ("c2s", "s2c"):
-      raise ValueError("Direction invalid.")
-    self.direction = direction
-    for (_, attrname) in filter(lambda d: d[1], self.get_datatable()):
-      setattr(self, attrname, kwargs[attrname])
+  def __init__(self, **kwargs):
+    kwset = set(kwargs.iterkeys())
+    is_c2s = kwset <= set(map(lambda p: p[1], self.data_c2s))
+    is_s2c = kwset <= set(map(lambda p: p[1],self.data_s2c))
+    if is_c2s:
+      for (_, attrname) in filter(lambda p: p[1], self.data_c2s):
+        setattr(self, attrname, kwargs[attrname])
+    if is_s2c:
+      for (_, attrname) in filter(lambda p: p[1], self.data_s2c):
+        setattr(self, attrname, kwargs[attrname])
+    if not is_c2s and not is_s2c:
+      raise ValueError("kwargs not match neither s2c or c2s direction.")
 
-  def get_datatable(self):
-    return getattr(self.__class__, "data_{}".format(self.direction))
-  
+  def __str__(self):
+    return str(self.__dict__)
 
+
+@register_packet_type
 class keep_alive(packet):
   id = 0x00
   data_c2s = data_s2c = (("i", "keepalive_id"), )
 
 
+@register_packet_type
 class login_request(packet):
   id = 0x01
   data_c2s = (
@@ -72,22 +125,26 @@ class login_request(packet):
   )
 
 
+@register_packet_type
 class handshake(packet):
   id = 0x02
   data_c2s = (("S", "username_host"), )
   data_s2c = (("S", "connection_hash"), )
 
 
+@register_packet_type
 class chat_message(packet):
   id = 0x03
   data_c2s = data_s2c = (("S", "message"), )
 
 
+@register_packet_type
 class time_update(packet):
   id = 0x04
   data_c2s = data_s2c = (("q", "time"), )
 
 
+@register_packet_type
 class entity_equipment(packet):
   id = 0x05
   data_s2c = (
@@ -98,6 +155,7 @@ class entity_equipment(packet):
   )
 
 
+@register_packet_type
 class spawn_position(packet):
   id = 0x06
   data_s2c = (
@@ -107,6 +165,7 @@ class spawn_position(packet):
   )
 
 
+@register_packet_type
 class use_entity(packet):
   id = 0x07
   data_c2s = (
@@ -116,6 +175,7 @@ class use_entity(packet):
   )
 
 
+@register_packet_type
 class update_health(packet):
   id = 0x08
   data_s2c = (
@@ -125,6 +185,7 @@ class update_health(packet):
   )
 
 
+@register_packet_type
 class respawn(packet):
   id = 0x09
   data_c2s = data_s2c = (
@@ -136,11 +197,13 @@ class respawn(packet):
   )
 
 
+@register_packet_type
 class player(packet):
   id = 0x0A
   data_c2s = (("?", "on_ground"), )
 
 
+@register_packet_type
 class player_position(player):
   id = 0x0B
   data_c2s = (
@@ -151,6 +214,7 @@ class player_position(player):
   ) + player.data_c2s
 
 
+@register_packet_type
 class player_look(player):
   id = 0x0C
   data_c2s = (
@@ -159,6 +223,7 @@ class player_look(player):
   ) + player.data_c2s
 
 
+@register_packet_type
 class player_position_look(player):
   id = 0x0D
   data_c2s = (
@@ -179,6 +244,7 @@ class player_position_look(player):
   ) + player.data_c2s
 
 
+@register_packet_type
 class player_digging(packet):
   id = 0x0E
   data_c2s = (
@@ -190,6 +256,7 @@ class player_digging(packet):
   )
 
 
+@register_packet_type
 class player_block_placement(packet):
   id = 0x0F
   data_c2s = (
@@ -201,11 +268,13 @@ class player_block_placement(packet):
   )
 
 
+@register_packet_type
 class held_item_change(packet):
   id = 0x10
   data_c2s = (("h", "slot_id"), )
 
 
+@register_packet_type
 class use_bed(packet):
   id = 0x11
   data_s2c = (
@@ -217,6 +286,7 @@ class use_bed(packet):
   )
 
 
+@register_packet_type
 class animation(packet):
   id = 0x12
   data_c2s = data_s2c = (
@@ -225,6 +295,7 @@ class animation(packet):
   )
 
 
+@register_packet_type
 class entity_action(packet):
   id = 0x13
   data_c2s = (
@@ -233,6 +304,7 @@ class entity_action(packet):
   )
 
 
+@register_packet_type
 class spawn_named_entity(packet):
   id = 0x14
   data_s2c = (
@@ -247,6 +319,7 @@ class spawn_named_entity(packet):
   )
 
 
+@register_packet_type
 class spawn_dropped_item(packet):
   id = 0x15
   data_s2c = (
@@ -263,6 +336,7 @@ class spawn_dropped_item(packet):
   )
 
 
+@register_packet_type
 class collect_item(packet):
   id = 0x16
   data_s2c = (
@@ -271,6 +345,7 @@ class collect_item(packet):
   )
 
 
+@register_packet_type
 class spawn_object_vehicle(packet):
   id = 0x17
   data_s2c = (
@@ -286,6 +361,7 @@ class spawn_object_vehicle(packet):
   )
 
 
+@register_packet_type
 class spawn_mob(packet):
   id = 0x18
   data_s2c = (
@@ -301,6 +377,7 @@ class spawn_mob(packet):
   )
 
 
+@register_packet_type
 class spawn_painting(packet):
   id = 0x19
   data_s2c = (
@@ -313,6 +390,7 @@ class spawn_painting(packet):
   )
 
 
+@register_packet_type
 class spawn_experience_orb(packet):
   id = 0x1A
   data_s2c = (
@@ -324,6 +402,7 @@ class spawn_experience_orb(packet):
   )
 
 
+@register_packet_type
 class entity_velocity(packet):
   id = 0x1C
   data_s2c = (
@@ -334,16 +413,19 @@ class entity_velocity(packet):
   )
 
 
+@register_packet_type
 class destroy_entity(packet):
   id = 0x1D
   data_s2c = (("i", "eid"), )
 
 
+@register_packet_type
 class entity(packet):
   id = 0x1E
   data_s2c = (("i", "eid"), )
 
 
+@register_packet_type
 class entity_relative_move(entity):
   id = 0x1F
   data_s2c = entity.data_s2c + (
@@ -353,6 +435,7 @@ class entity_relative_move(entity):
   )
 
 
+@register_packet_type
 class entity_look(entity):
   id = 0x20
   data_s2c = entity.data_s2c + (
@@ -361,11 +444,13 @@ class entity_look(entity):
   )
 
 
+@register_packet_type
 class entity_relative_move_look(entity):
   id = 0x21
   data_s2c = entity.data_s2c + entity_relative_move.data_s2c[1:] + entity_look.data_s2c[1:]
 
 
+@register_packet_type
 class entity_teleport(entity):
   id = 0x22
   data_s2c = entity.data_s2c + (
@@ -377,26 +462,31 @@ class entity_teleport(entity):
   )
 
 
+@register_packet_type
 class entity_head_look(entity):
   id = 0x23
   data_s2c = entity.data_s2c + (("b", "head_yaw"), )
 
 
+@register_packet_type
 class entity_statys(entity):
   id = 0x26
   data_s2c = entity.data_s2c + (("b", "entity_status"), )
 
 
+@register_packet_type
 class attach_entity(entity):
   id = 0x27
   data_s2c = entity.data_s2c + (("i", "vehical_id"), )
 
 
+@register_packet_type
 class entity_metadata(entity):
   id = 0x28
   data_s2c = entity.data_s2c + (("M", "metadata"), )
 
 
+@register_packet_type
 class entity_effect(entity):
   id = 0x29
   data_s2c = entity.data_s2c + (
@@ -406,11 +496,13 @@ class entity_effect(entity):
   )
 
 
+@register_packet_type
 class remove_entity_effect(entity):
   id = 0x2A
   data_s2c = entity.data_s2c + (("b", "effect_id"), )
 
 
+@register_packet_type
 class set_experience(packet):
   id = 0x2B
   data_s2c = (
@@ -420,6 +512,7 @@ class set_experience(packet):
   )
 
 
+@register_packet_type
 class map_column_allocation(packet):
   id = 0x32
   data_s2c = (
@@ -429,6 +522,7 @@ class map_column_allocation(packet):
   )
 
 
+@register_packet_type
 class map_chunks(packet):
   id = 0x33
   data_s2c = (
@@ -443,6 +537,7 @@ class map_chunks(packet):
   )
 
 
+@register_packet_type
 class multi_block_change(packet):
   id = 0x34
   data_s2c = (
@@ -454,6 +549,7 @@ class multi_block_change(packet):
   )
 
 
+@register_packet_type
 class block_change(packet):
   id = 0x35
   data_s2c = (
@@ -465,6 +561,7 @@ class block_change(packet):
   )
 
 
+@register_packet_type
 class block_action(packet):
   id = 0x36
   data_s2c = (
@@ -476,6 +573,7 @@ class block_action(packet):
   )
 
 
+@register_packet_type
 class explosion(packet):
   id = 0x3C
   data_s2c = (
@@ -488,6 +586,7 @@ class explosion(packet):
   )
 
 
+@register_packet_type
 class sound_partical_effect(packet):
   id = 0x3D
   data_s2c = (
@@ -499,6 +598,7 @@ class sound_partical_effect(packet):
   )
 
 
+@register_packet_type
 class change_game_state(packet):
   id = 0x46
   data_s2c = (
@@ -507,6 +607,7 @@ class change_game_state(packet):
   )
 
 
+@register_packet_type
 class thunderbolt(packet):
   id = 0x47
   data_s2c = (
@@ -518,6 +619,7 @@ class thunderbolt(packet):
   )
 
 
+@register_packet_type
 class open_window(packet):
   id = 0x64
   data_s2c = (
@@ -528,11 +630,13 @@ class open_window(packet):
   )
 
 
+@register_packet_type
 class close_window(packet):
   id = 0x65
   data_c2s = data_s2c = (("b", "window_id"), )
 
 
+@register_packet_type
 class click_window(packet):
   id = 0x66
   data_c2s = (
@@ -545,6 +649,7 @@ class click_window(packet):
   )
 
 
+@register_packet_type
 class set_slot(packet):
   id = 0x67
   data_s2c = (
@@ -554,6 +659,7 @@ class set_slot(packet):
   )
 
 
+@register_packet_type
 class set_window_items(packet):
   id = 0x68
   data_s2c = (
@@ -563,6 +669,7 @@ class set_window_items(packet):
   )
 
 
+@register_packet_type
 class update_window_property(packet):
   id = 0x69
   data_s2c = (
@@ -572,6 +679,7 @@ class update_window_property(packet):
   )
 
 
+@register_packet_type
 class confirm_transaction(packet):
   id = 0x6A
   data_c2s = data_s2c = (
@@ -581,6 +689,7 @@ class confirm_transaction(packet):
   )
 
 
+@register_packet_type
 class creative_inventory_action(packet):
   id = 0x6B
   data_c2s = data_s2c = (
@@ -589,6 +698,7 @@ class creative_inventory_action(packet):
   )
 
 
+@register_packet_type
 class enchant_item(packet):
   id = 0x6C
   data_c2s = (
@@ -597,6 +707,7 @@ class enchant_item(packet):
   )
 
 
+@register_packet_type
 class update_sign(packet):
   id = 0x82
   data_c2s = data_s2c = (
@@ -610,6 +721,7 @@ class update_sign(packet):
   )
 
 
+@register_packet_type
 class item_data(packet):
   id = 0x83
   data_s2c = (
@@ -620,6 +732,7 @@ class item_data(packet):
   )
 
 
+@register_packet_type
 class update_title_entity(packet):
   id = 0x84
   data_s2c = (
@@ -633,6 +746,7 @@ class update_title_entity(packet):
   )
 
 
+@register_packet_type
 class increment_statistic(packet):
   id = 0xC8
   data_s2c = (
@@ -641,6 +755,7 @@ class increment_statistic(packet):
   )
 
 
+@register_packet_type
 class player_list_item(packet):
   id = 0xC9
   data_s2c = (
@@ -650,6 +765,7 @@ class player_list_item(packet):
   )
 
 
+@register_packet_type
 class player_abilities(packet):
   id = 0xCA
   data_c2s = data_s2c = (
@@ -660,6 +776,7 @@ class player_abilities(packet):
   )
 
 
+@register_packet_type
 class plugin_message(packet):
   id = 0xFA
   data_c2s = data_s2c = (
@@ -669,11 +786,13 @@ class plugin_message(packet):
   )
 
 
+@register_packet_type
 class server_list_ping(packet):
   id = 0xFE
   data_c2s = ()
 
 
+@register_packet_type
 class disconnect(packet):
   id = 0xFF
   data_c2s = data_s2c = (("S", "reason"), )
--- mc/robot.py
+++ mc/robot.py
@@ -1,23 +1,28 @@
-import socket
 import struct
 
+from mc import net
 from mc.packets import *
 
 class robot(object):
 
   def __init__(self):
-    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    pass
+    self.socket = net.mcsocket()
+    # packetreceiver is a thread, receiving packets and updating the user information.
+    # self.packetreceiver = 
 
   def login(self, host, port, user, password=""):
     self.socket.connect((host, port))
-    self.socket.send(pack(handshake("c2s", username_host="{};{}:{}".format(user, host, port))))
-    recv = self.socket.recv(1024)
-    self.socket.send(pack(login_request("c2s", version=29, username=user)))
-    recv = self.socket.recv(1024)
+    self.socket.sendmc(handshake(username_host="{};{}:{}".format(user, host, port)))
+    recv = self.socket.recvmc()
+    print recv
+    self.socket.sendmc(login_request(version=29, username=user))
+    recv = self.socket.recvmc()
+    print recv
+    # self.packetreceiver.start()
 
   def logout(self, message=""):
-    self.socket.send(pack(disconnect("c2s", reason=message)))
+    # self.packetreceiver.stop()
+    self.socket.sendmc(disconnect(reason=message))
     self.socket.close()
 
   def getPosition(self):
