99d2e653737e6a965a76198be0a980f798c9a0a2
cwahbong <cw.ahbong@gmail.com>
2012-07-30 00:00:09 +0800

    Remove unnessesary methods in util.py.

--- mc/util.py
+++ mc/util.py
@@ -20,34 +20,3 @@ class repeater(threading.Thread):
   def stop_later(self):
     self.__stop = True
 
-
-def pack_single(fmt, arg):
-  if fmt=="S":
-    return String.pack(arg)
-  elif fmt=="T":
-    return Slot.pack(arg)
-  elif fmt=="M":
-    return MetaData.pack(arg)
-  elif fmt in "bB?hHiIlLqQfd":
-    attr = 0 if arg is None else arg
-    nfmt = "!{}".format(fmt)
-    result = struct.pack(nfmt, attr)
-  else:
-    raise ValueError("Invalid fmt '{}'.".format(fmt))
-  return result
-
-
-def unpack_from_single(fmt, buffer, offset=0):
-  if fmt=="S":
-    return String.unpack(buffer, offset)
-  elif fmt=="T":
-    return Slot.unpack(buffer, offset)
-  elif fmt=="M":
-    return MetaData.unpack(buffer, offset)
-  elif fmt in "bB?hHiIlLqQfd":
-    result = struct.unpack_from("!{}".format(fmt), buffer, offset)[0]
-    offset += struct.calcsize(fmt)
-  else:
-    raise ValueError("Invalid fmt '{}'.".format(fmt))
-  return result, offset
-
