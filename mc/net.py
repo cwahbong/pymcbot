1aa7e1a31af43858d61cfb50539642b2fc3c6156
cwahbong <cw.ahbong@gmail.com>
2012-07-30 20:22:23 +0800

    Fix a bug when receiving packet.
    Give it a bigger fetch size such that larger packet can be received
    correctly.

--- mc/net.py
+++ mc/net.py
@@ -6,6 +6,7 @@ from mc import packets
 
 
 class mcsocket(object):
+  max_fetch = 16384
 
   def __init__(self):
     self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -22,18 +23,13 @@ class mcsocket(object):
     return self.__socket.send(packets.pack(packet))
 
   def recvmc(self):
-    try:
-      if len(self.__buf)==0:
-        self.__buf += self.__socket.recv(256)
-      elif len(self.__buf)<256:
-        self.__buf += self.__socket.recv(256, socket.MSG_DONTWAIT)
-    except socket.error as e:
-      if e.errno!=errno.EAGAIN:
-        raise e
-      time.sleep(0.05)
+    if len(self.__buf)==0:
+      self.__buf += self.__socket.recv(self.max_fetch)
     if len(self.__buf)==0:
       return None
     p, size = packets.unpack(self.__buf)
     self.__buf = self.__buf[size:]
+    if p.id==51:
+      print p.__dict__, len(self.__buf)
     return p
 
