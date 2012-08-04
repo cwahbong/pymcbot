a66f401b807adabdbcf0c1876a1511746b7ff485
cwahbong <cw.ahbong@gmail.com>
2012-08-05 00:33:40 +0800

    Fix a but that will accumulate data in the receive buffer.

--- mc/net.py
+++ mc/net.py
@@ -6,8 +6,7 @@ from mc import packets
 
 
 class mcsocket(object):
-  threshold = 8192
-  max_fetch = 16384
+  origin_fetch = 16
 
   def __init__(self):
     self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,18 +20,23 @@ class mcsocket(object):
     self.__socket.close()
 
   def sendmc(self, packet):
+    print "send:", packet.name()
     return self.__socket.send(packets.pack(packet))
 
   def recvmc(self):
+    again = False
+    fetch = self.origin_fetch
     while True:
       try:
-        if len(self.__buf)<self.threshold:
-          self.__buf += self.__socket.recv(self.max_fetch)
-        if len(self.__buf)==0:
-          return None
+        if again or len(self.__buf)==0:
+          self.__buf += self.__socket.recv(fetch)
+          if len(self.__buf)==0:
+            return None
         p, size = packets.unpack(self.__buf)
         self.__buf = self.__buf[size:]
+        print "receive:", p.name(), len(self.__buf)
         return p
       except Exception as e:
-        pass
+        again = True
+        fetch *= 2
 
